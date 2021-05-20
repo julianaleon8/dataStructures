3de1096d2f7c7cad89afdc6b2252ad16f0810da4
Juliana Leon <julianaleon8@gmail.com>
2021-05-19 21:22:26 -0400

    operations on arrays, all sorting

--- Main.cpp
+++ Main.cpp
@@ -2,17 +2,14 @@
 #include "array.cpp"
 
 int main() {
-    int a[4] = {1,7,4,5};
+    int a[8] = {1,7,4,5, 0 ,3 ,4,5};
     unsigned int len = sizeof(a)/sizeof(a[0]);
 
-    int pos = linealSearch<int>(a, 6, len);
-    mergeSortArrayRe<int>(a, 0, len-1);
-    pos = binarySearch<int>(a, 6, len);
-
+    heapSortArrayIt<int>(a, len);
+    
     for (int i = 0; i < len ; i++) {
         std::cout << " " << a[i];    
     }
-
-    //std::cout << "posicion of elem " << pos;
+    
     return 0;
 }
--- array.cpp
+++ array.cpp
@@ -1,4 +1,120 @@
 #include <iostream>
+#include <stack>
+
+/** 
+ * Build a min-heap array structure from an array
+ * that does not satisfy the heap property.
+ * This is a recursive algorithm.
+ * 
+ * Left child of i-th node is at (2*i + 1)th index.
+ * Right child of i-th node is at (2*i + 2)th index.
+ * Parent of i-th node is at (i-1)/2 index.
+ * 
+ * @param a: Input array to be heapify
+ * @param n: numbers of nodes (size on the array)
+ */
+template < typename T>
+void heapify(T a[], int index, int n) {
+    int currentParent = index;
+    int leftChild = 2 * index + 1;
+    int rightChild = 2 * index + 2;
+    // check left child
+    // Change here for converting into a max-heap
+    if (leftChild < n && a[currentParent] > a[leftChild]) {
+        currentParent = leftChild;
+    }
+    // Change here for converting into a max-heap
+    if (rightChild < n && a[currentParent] > a[rightChild]) {
+        currentParent = rightChild;
+    }
+
+    if (index != currentParent) {
+        std::swap(a[currentParent], a[index]);
+        heapify(a, currentParent, n);
+    }
+}
+
+/** 
+ * Build a min-heap array structure from an array
+ * that does not satisfy the heap property.
+ * This is a iterative algorithm.
+ * 
+ * Left child of i-th node is at (2*i + 1)th index.
+ * Right child of i-th node is at (2*i + 2)th index.
+ * Parent of i-th node is at (i-1)/2 index.
+ * 
+ * @param a: Input array to be heapify
+ * @param n: numbers of nodes (size on the array)
+ */
+template < typename T>
+void heapifyIt(T a[], int index, int n) {
+    while (1) {
+        int currentParent = index;
+        int leftChild = 2 * index + 1;
+        int rightChild = 2 * index + 2;
+        // check left child
+        if (leftChild < n && a[currentParent] > a[leftChild]) {
+            currentParent = leftChild;
+        }
+        if (rightChild < n && a[currentParent] > a[rightChild]) {
+            currentParent = rightChild;
+        }
+        if (index == currentParent) {
+            break;
+        }
+
+        std::swap(a[currentParent], a[index]);
+        index = currentParent;
+    
+    }
+}
+
+/**
+ * Sort the array using the min-heap structure
+ * recursively
+ * @param a: Input array to be sorted 
+ * @param n: Size of the array
+ */
+template < typename T>
+void heapSortArray(T a[], int len) {
+    // build heap
+    for (int i = len -1; i >= 0; i--) {
+        heapify(a, i,len);
+    }
+
+    for (int j = len -1; j >= 0; j--) {
+        std::swap(a[j], a[0]);
+        heapify(a, 0 , j);
+    }
+
+    for (int k = 0; k <len ; k++) {
+        std::cout << " " << a[k];
+    }
+}
+
+/**
+ * Sort the array using the min-heap structure
+ * iterative
+ * @param a: Input array to be sorted 
+ * @param n: Size of the array
+ */
+template < typename T>
+void heapSortArrayIt(T a[], int len) {
+    // build heap
+    for (int i = len -1; i >= 0; i--) {
+        heapifyIt(a, i,len);
+    }
+
+    for (int j = len -1; j >= 0; j--) {
+        std::swap(a[j], a[0]);
+        heapifyIt(a, 0 , j);
+    }
+
+    for (int k = 0; k <len ; k++) {
+        std::cout << " " << a[k];
+    }
+}
+
 /**
  * Sort the array using the selection algorithm
  *
@@ -43,7 +159,7 @@ void insertionSortArray(T a[], unsigned int len) {
 /**
  * Merge two arrays where the smaller elements
  * comes first
- *
+ * a < b
  * @param a: Final array after the merge
  * @param low: Smaller index of the array 
  * @param mid: Middle index of the array
@@ -51,7 +167,7 @@ void insertionSortArray(T a[], unsigned int len) {
  */
 
 template < typename T>
-void mergeSortRe(T a[], int low, int mid, int high) {
+void mergeSort(T a[], int low, int mid, int high) {
     int n1 = mid - low + 1;
     int n2 = high - mid;
     int b[n1];
@@ -106,31 +222,146 @@ void mergeSortArrayRe(T a[], int low, int high) {
     int mid = (low+high)/2;
     mergeSortArrayRe(a, low, mid );
     mergeSortArrayRe(a, mid+1, high );
-    mergeSortRe(a, low, mid, high);
+    mergeSort(a, low, mid, high);
+}
+
+/**
+ * Sort the array using the merge algorithm
+ * iterative
+ * @param a: Array to be sorted 
+ * @param low: Smaller index of the array 
+ * @param high: Greater index of the array
+ */
+template < typename T>
+void mergeSortArrayIt(T a[], int low, int high) {
+    std::stack<int> s;
+    int len = high;
+    int min = low;
+    s.push(low);
+    s.push(high);
+    while (!s.empty()) {
+        
+        high = s.top();
+        s.pop();
+        low =  s.top();
+        s.pop();
+        int mid = (low + high) / 2;
+        
+        if (mid > low) {
+            s.push(low);
+            s.push(mid);
+        } 
+        if ( mid + 1 < high) {
+            s.push(mid + 1);
+            s.push(high);
+        }
+        mergeSort(a, low, mid, high);
+    }
+    int mid = (len + min) /2;
+    mergeSort(a, min, mid, len);
+}
+
+/**
+ * Partitions the array putting all smallest 
+ * elements than the pivot to the left and the 
+ * greater elements to the right of the pivot
+ * The pivot is the middle of the array
+ * @param a: Array to be sorted 
+ * @param low: Smaller index of the array 
+ * @param high: Greater index of the array
+ */
+template < typename T>
+int partitionM(T a[], int low, int high) {
+    int mid = (low + high)/2;
+    int pivot = a[mid];
+    int i = low;
+    int j = high;
+    while (1) { 
+        while (a[i] < pivot) {
+            i++;
+        }
+        while (a[j] > pivot) {
+            j--;
+        }
+
+        if (i >= j) {
+            return j;
+        }
+        std::swap(a[j], a[i]);
+    }
+}
+
+/**
+ * Partitions the array putting all smallest 
+ * elements than the pivot to the left and the 
+ * greater elements thighesto the right of the pivot
+ * The pivot is last the  element of the array
+ *
+ * @param a: Array to be sorted 
+ * @param low: Smaller index of the array 
+ * @param high: Greater index of the array
+ */
+template < typename T>
+int partition(T a[], int low, int high) {
+    int pivot = a[high];
+    int relPosPivot = low;
+    for (int j = low; j < high ; j++) {
+        if (a[j] < pivot) {
+            std::swap(a[j], a[relPosPivot]);
+            relPosPivot++;
+        }
+    }
+    std::swap(a[high], a[relPosPivot]);
+    return relPosPivot;
 }
 
 /**
  * Sort the array using the quick sort algorithm
- *
+ * This is a recursive algorithm.
  * @param a: Array to be sorted 
  * @param low: Smaller index of the array 
  * @param high: Greater index of the array
  */
 template < typename T>
 void quickSortArrayRe(T a[], int low, int high) {
-    return;
+    if (low >= high) {
+        return;
+    }
+
+    int pos = partition(a, low, high);
+    quickSortArrayRe(a, low, pos-1);
+    quickSortArrayRe(a, pos +1, high);
 }
 
 /**
- * Sort the array using the heap sort algorithm
- *
+ * Sort the array using the quick sort algorithm
+ * This is a iterative algorithm.
  * @param a: Array to be sorted 
  * @param low: Smaller index of the array 
  * @param high: Greater index of the array
  */
 template < typename T>
-void heapSortArrayRe(T a[], unsigned int len) {
-    return;
+void quickSortArrayIt(T a[], int low, int high) {
+    
+    std::stack<int> s;
+    s.push(low);
+    s.push(high);
+    
+    while (!s.empty()) {
+        high = s.top();
+        s.pop();
+        low =  s.top();
+        s.pop();
+        int pos = partition(a, low, high);
+        if (pos - 1 > low) {
+            s.push(low);
+            s.push(pos -1);
+        } 
+        if ( pos + 1 < high) {
+            s.push(pos + 1);
+            s.push(high);
+        }
+    }
 }
 
 /**
